719a1d7db4a6050b851f099fcb8b0cae683ed5d2
chuck francis <chuck_francis10@yahoo.com>
2020-07-21 15:19:20 -0400

    Add project files.

---
+++ projecgutenberg1.1.sln/UnitTest1.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace projecgutenberg1._1.sln
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Did you know that, in 1440 or thereabouts, ");
+            Console.WriteLine("Johannes Gutenberg invented the printing press? ");
+            Console.WriteLine("He started out as a goldsmith!");
+            Console.WriteLine("His invention made rapid and prolific typed volumes ");
+            Console.WriteLine("available for the first time ever! ");
+            Console.WriteLine("We are like a modern Gutenberg, ");
+            Console.WriteLine("printing vast amounts to the waiting console with ease! ");
+        }
+    }
+}
